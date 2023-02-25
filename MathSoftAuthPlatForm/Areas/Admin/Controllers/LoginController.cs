a375a0f34461ea4776f11d740f44b691b1de7f16
Maliang.China <42198953+jidanji@users.noreply.github.com>
2023-02-25 21:32:10 +0800

    判断当前用户是否在登录
    判断当前用户是否在登录

--- MathSoftAuthPlatForm/Areas/Admin/Controllers/LoginController.cs
+++ MathSoftAuthPlatForm/Areas/Admin/Controllers/LoginController.cs
@@ -10,7 +10,7 @@ using System.Linq;
 using System.Web.Mvc;
 using System.Web.Security;
 
- 
+
 
 namespace MAZIKONG.Areas.Admin.Controllers
 {
@@ -23,12 +23,18 @@ namespace MAZIKONG.Areas.Admin.Controllers
             ViewBag.BrowserType = b.Type;
             int toal = 0;
             Guid idj = Guid.Parse("0D930AF9-76A3-4AB8-A492-580216BA64CC");
-          
 
+            FormsAuthentication.SignOut();
             return View();
         }
 
-
+        #region 根据用户名和密码验证用户
+        /// <summary>
+        /// 根据用户名和密码验证用户
+        /// </summary>
+        /// <param name="UserName"></param>
+        /// <param name="UserPWD"></param>
+        /// <returns></returns>
         public ActionResult ValidUser(String UserName, string UserPWD)
         {
             UIModelData<String> obj = null;
@@ -55,7 +61,7 @@ namespace MAZIKONG.Areas.Admin.Controllers
                     //Response.Write("<hr>");
 
 
-                   
+
 
 
                     FormsAuthentication.SetAuthCookie(UserName, false);
@@ -94,5 +100,68 @@ namespace MAZIKONG.Areas.Admin.Controllers
             }
             return new JsonResult { Data = obj, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
         }
+        #endregion
+
+        #region 根据cookie验证当前用户的登录信息
+        /// <summary>
+        /// 根据cookie验证当前用户的登录信息
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult CheckCurrentLoginStatus()
+        {
+            string username = System.Web.HttpContext.Current.User.Identity.Name;
+
+
+            if (string.IsNullOrWhiteSpace(username))
+            {
+                return new JsonResult
+                {
+                    Data = new UIModelData<string>
+                    {
+                        Data = string.Empty,
+                        remark = "当前用户没有登录",
+                        suc = false
+                    },
+                    JsonRequestBehavior = JsonRequestBehavior.AllowGet
+                };
+            }
+            else
+            {
+                int total = 0;
+                List<Math_UserInfo> list = new BLL_User().Search(-1, -1, out total, i => i.UserAccount == username);
+                if (list.Count() == 0)
+                {
+                    return new JsonResult
+                    {
+                        Data = new UIModelData<string>
+                        {
+                            Data = string.Empty,
+                            remark = "当前用户没有登录",
+                            suc = false
+                        },
+                        JsonRequestBehavior = JsonRequestBehavior.AllowGet
+                    };
+                }
+                else
+                {
+                    Math_UserInfo modal = list.FirstOrDefault();
+                    UIMathUserInfo modal1 = Mapper.Map<UIMathUserInfo>(modal);
+
+
+                    return new JsonResult
+                    {
+                        Data = new UIModelData<UIMathUserInfo>
+                        {
+                            Data = modal1,
+                            remark = string.Empty,
+                            suc = true
+                        },
+                        JsonRequestBehavior = JsonRequestBehavior.AllowGet
+                    };
+                }
+            }
+
+        }
+        #endregion
     }
 }
